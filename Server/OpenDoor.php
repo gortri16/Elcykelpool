ecf58d5b9422341453bc3333827f419e8a61831f
gortri16 <46687769+gortri16@users.noreply.github.com>
2019-01-14 19:53:33 +0100

    Update OpenDoor.php

--- Server/OpenDoor.php
+++ Server/OpenDoor.php
@@ -1,13 +1,17 @@
 <?php
+//establish connection to database
 $servername = "localhost";
 $username = "root";
 $pasword = "elcykel";
 $wordpress = "wordpress";
 $conn = new mysqli($servername,$username,$pasword,$wordpress);
+// get the current online user
 $current_user = wp_get_current_user();
+//check if connection was established
 if($conn->connect_error){
     die("Connection failed: " . $conn->connect_error);
 }
+//Check which bike that was ordered by current user.
 $select = "SELECT cykelID FROM Orders WHERE userID = '$current_user->ID' AND active = TRUE";
 $result = $conn->query($select);
 if($result->num_rows>0){
@@ -15,6 +19,7 @@ if($result->num_rows>0){
    $cykelid = $row["cykelID"];
   }
 }  
+// open the door for the bike that was booked
 $opendoor = "UPDATE Cykel_Fack SET doorlocked = TRUE WHERE cykelID = $cykelid";
 if($result2 = $conn->query($opendoor)){
     echo " dörren öppnas ";
