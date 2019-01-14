58b8e78c661cd8f5e5c93ec3cb3aa72d1a1d31b1
gortri16 <46687769+gortri16@users.noreply.github.com>
2019-01-14 19:50:55 +0100

    Update OrderHistorik.php

--- Server/OrderHistorik.php
+++ Server/OrderHistorik.php
@@ -1,13 +1,17 @@
 <?php
+//connecting to database
 $servername = "localhost";
 $username = "root";
 $pasword = "elcykel";
 $wordpress = "wordpress";
 $conn = new mysqli($servername,$username,$pasword,$wordpress);
+//get the current online user
 $current_user = wp_get_current_user();
+//check if a connection was established
 if($conn->connect_error){
     die("Connection failed: " . $conn->connect_error);
 }
+//Select the last 10 orders the current user has made and display them starting from latest to last.
 $select = "SELECT orderID,dateCreated,dateEnded,cykelID,active FROM Orders WHERE userID = '$current_user->ID' ORDER BY orderID DESC LIMIT 10";
 $result = $conn->query($select);
 if($result->num_rows>0){
